f0614384d2f7ccb09c13e9c3c0b3993411d02ebb
K DHYANA SAMAGA <156940188+Dhyana2004@users.noreply.github.com>
2024-07-14 19:22:20 +0530

    Prims algorithm.c

---
+++ Prims algorithm.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define INF 999
+
+int main() {
+    int n, cost[10][10], visited[10] = {1}, mincost = 0;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0 && i != j) cost[i][j] = INF;
+        }
+
+    printf("\nEdges in the Minimum Spanning Tree:\n");
+    for (int edges = 1; edges < n; edges++) {
+        int min = INF, u = -1, v = -1;
+        for (int i = 0; i < n; i++) {
+            if (visited[i])
+                for (int j = 0; j < n; j++)
+                    if (!visited[j] && cost[i][j] < min) {
+                        min = cost[i][j];
+                        u = i; v = j;
+                    }
+        }
+        if (u != -1) {
+            printf("Edge %d: (%d %d) cost: %d\n", edges, u + 1, v + 1, min);
+            mincost += min;
+            visited[v] = 1;
+        }
+    }
+    printf("Minimum cost = %d\n", mincost);
+    return 0;
+}
